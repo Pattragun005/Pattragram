3786c61bae73ba19c0c78fb49ff7fad2d9af0a15
Pattragun Janpan <s5830213005@phuket.psu.ac.th>
2017-10-23 18:24:19 +0700

    version 2.0.2

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
+
+//create
+var url = "http://localhost:3000/posts";
+  $("#postpin").click(function () {
+
+    var budget = $("#budget").val();
+    var way2go = $("#way2go").val();
+    var item = $("#item").val();
+    var beware = $("#beware").val();
+
+    $.post(url, {
+      
+      budget : budget,
+      way2go : way2go,
+      item : item,
+      beware: beware
+     
+    });
+    alert('completed');
+    setTimeout(window.location.href = "index.html", 1000);
+  });
+
+
+
+
+
 var url = "http://localhost:3000/posts";
 $.get(url, function (data) {
   var template = $('#template').html();
@@ -8,7 +34,7 @@ $.get(url, function (data) {
     $("#timeline").append(rendered);
   }
 });});
-
+/*
 document.addEventListener('init', function (event) {
 
   var page = event.target;
@@ -19,10 +45,10 @@ document.addEventListener('init', function (event) {
     generateStoryBubbles(stories);
   }
 
-});
+}); */
 
 //The show event listener does the same thing as the one above but on the search page when it's shown.
-
+/*
 document.addEventListener('show', function (event) {
   var page = event.target;
 
@@ -31,13 +57,13 @@ document.addEventListener('show', function (event) {
 
     generateStoryBubbles(channels);
   }
-});
+}); */
 
 /*
 * This function is used to toggle the grid/list display of the posts in the profile page as well as
 * change the color of the buttons to show which is the current view.
 */
-
+/*
 function display(id) {
   document.getElementById("list").style.color = "#1f1f21";
   document.getElementById("grid").style.color = "#1f1f21";
@@ -47,9 +73,9 @@ function display(id) {
   document.getElementById("grid_view").style.display = "none";
   document.getElementById(id + "_view").style.display = "block";
 }
-
+*/
 //The generateStoryBubbles function is used to create the carousel items be used as stories by the upper two events.
-
+/*
 function generateStoryBubbles(element) {
   for (var i = 0; i < 9; i++) {
     element.appendChild(ons.createElement(
@@ -61,10 +87,10 @@ function generateStoryBubbles(element) {
       '</ons-carousel-item>'
     ));
   }
-}
+} */
 
 //The Like function is used to make the white heart appear in front of the picture as well as make the like button into a red heart and vice versa.
-
+/*
 var like = function (num) {
   if (document.getElementById("button-post-like-" + num).classList.contains("like")) {
     document.getElementById("button-post-like-" + num).classList.remove("ion-ios-heart", "like");
@@ -78,14 +104,11 @@ var like = function (num) {
       document.getElementById("post-like-" + num).style.opacity = 0;
     }, 600);
   }
-}
+}*/
 
 //The readStory function is used to change the red circle around a new story into grey after tapping on the new storry (thus reading it)
-
+/*
 var readStory = function (event) {
   event.parentNode.classList.remove("unread");
   event.parentNode.classList.add("read");
-}
-
-
-  
+}*/
